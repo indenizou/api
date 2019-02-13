7bd843917d589757d269fa34e70c9944466ddc62
christhopher.leao <christhopherleao@icloud.com>
2019-02-13 03:32:25 -0200

    remove authorization from req headers

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.options('*', cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept');
   next();
 });
 app.use(bodyParser.json({ limit: '5mb' }));
